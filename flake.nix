decff5e0e90a49881e809922e33ff0b13a77fdb6
Tristan Ross <tristan.ross@midstall.com>
2022-08-27 15:36:46 -0700

    feat: working on nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,14 @@
   description = "A libadwaita wrapper for ExpidusOS with Tokyo Night's styling";
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "i686-linux"
+        "riscv64-linux"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
@@ -21,6 +28,36 @@
             buildInputs = with pkgs; [ libadwaita ];
 
             enableParallelBuilding = true;
+
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/ExpidusOS/libtokyo";
+              license = with licenses; [ gpl3Only ];
+              maintainers = [ "Tristan Ross" ];
+            };
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+              vala
+              nodejs
+              gcc
+              libadwaita.dev
+              libadwaita.devdoc
+            ];
+
+            shellHooks = ''
+              export PATH="$PWD/node_modules/.bin/:$PATH"
+              alias run="npm run"
+            '';
           };
         });
     };
