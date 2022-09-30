c300d4e651d10b432159404b40db7bee99862059
Tristan Ross <tristan.ross@midstall.com>
2022-09-29 17:53:39 -0700

    feat: extract adwaita theme to make gtk4 theme

---
+++ scripts/patch-adwaita-styles.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+
+if (process.argv.length !== 4) {
+  console.error("4 arguments are required");
+  process.exit(1);
+}
+
+fs.writeFileSync(process.argv[3], fs.readFileSync(process.argv[2], "utf-8")
+  .split('\n')
+  .filter((str) => !str.startsWith("@define-color"))
+  .map((str) => {
+    for (const match of str.matchAll(/@([a-zA-Z_]+)/g)) {
+      if (match.index == 0) continue;
+      str = str.replace(match[0], `gtkcolor(${match[1]})`);
+    }
+    return str.split('alpha(').join('gtkalpha(')
+      .split('mix(').join('gtkmix(');
+  }).join('\n'));
