42971b8f3c36a115aba6acd2db379e98e5739109
Tristan Ross <tristan.ross@midstall.com>
2022-12-01 14:12:40 -0800

    feat: update flake to use new submodule fetcher

--- flake.nix
+++ flake.nix
@@ -5,5 +5,17 @@
     url = github:ExpidusOS/sdk;
   };
 
-  outputs = { self, expidus-sdk }: expidus-sdk.lib.expidus.flake.makeOverride { inherit self; name = "libtokyo"; };
+  inputs.adw-gtk3 = {
+    url = github:lassekongo83/adw-gtk3;
+    flake = false;
+  };
+
+  outputs = { self, expidus-sdk, adw-gtk3 }:
+    with expidus-sdk.lib;
+    expidus.flake.makeOverride {
+      self = expidus.flake.makeSubmodules self {
+        "gtk3/deps/adw-gtk3" = adw-gtk3;
+      };
+      name = "libtokyo";
+    };
 }
