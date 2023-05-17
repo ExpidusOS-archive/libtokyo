86af6d2f2ab93a4d14d54d9402ab3d6db5717c11
Tristan Ross <tristan.ross@midstall.com>
2023-05-17 16:57:32 -0700

    chore(libtokyo): full coverage on color

--- packages/libtokyo/test/color_test.dart
+++ packages/libtokyo/test/color_test.dart
@@ -4,31 +4,34 @@ import 'package:libtokyo/color.dart';
 final String _TEST_COLOR = 'a1b2c3d4';
 
 void testColor(Color color, double red, double green, double blue, double alpha, String str) {
-  expect(color.red, red);
-  expect(color.green, green);
-  expect(color.blue, blue);
-  expect(color.alpha, alpha);
+  expect(color.red, equals(red));
+  expect(color.green, equals(green));
+  expect(color.blue, equals(blue));
+  expect(color.alpha, equals(alpha));
 
-  expect(color[0], color.red);
-  expect(color[1], color.green);
-  expect(color[2], color.blue);
-  expect(color[3], color.alpha);
+  expect(color[0], equals(color.red));
+  expect(color[1], equals(color.green));
+  expect(color[2], equals(color.blue));
+  expect(color[3], equals(color.alpha));
+  expect(() => color[4], throwsA(isRangeError));
 
-  expect(color.toString(), str);
+  expect(color.toString(), equals(str));
+  expect(color, equals(Color(red, green, blue, alpha)));
 }
 
 void main() {
-  test('Color.fromString with starting #', () {
+  test('fromString with starting #', () {
     final color = Color.fromString('#${_TEST_COLOR}');
+
     testColor(color, 0xa1 / 255, 0xb2 / 255, 0xc3 / 255, 0xd4 / 255, "#${_TEST_COLOR}");
   });
 
-  test('Color.fromString without starting #', () {
+  test('fromString without starting #', () {
     final color = Color.fromString('${_TEST_COLOR}');
     testColor(color, 0xa1 / 255, 0xb2 / 255, 0xc3 / 255, 0xd4 / 255, "#${_TEST_COLOR}");
   });
 
-  test('Color.fromJSON', () {
+  test('fromJSON', () {
     final color = Color.fromJSON('"#${_TEST_COLOR}"');
     testColor(color, 0xa1 / 255, 0xb2 / 255, 0xc3 / 255, 0xd4 / 255, "#${_TEST_COLOR}");
   });
