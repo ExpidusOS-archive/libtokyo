209f65e79136192e5f1b82e6806d0400759200ec
Tristan Ross <tristan.ross@midstall.com>
2023-05-21 16:14:20 -0700

    chore(libtokyo_flutter): improve padding on window_bar

--- packages/libtokyo_flutter/lib/widgets/window_bar.dart
+++ packages/libtokyo_flutter/lib/widgets/window_bar.dart
@@ -1,6 +1,9 @@
 import 'package:libtokyo/libtokyo.dart' as libtokyo;
 import 'package:flutter/material.dart';
 
+final _LEADING_ICON_PADDING_SIZE_MULT = 1.0;
+final _ICON_PADDING_SIZE_MULT = 0.25;
+
 class WindowBar extends StatelessWidget implements libtokyo.WindowBar<Key, Widget>, PreferredSizeWidget {
   const WindowBar({
     super.key,
@@ -23,16 +26,21 @@ class WindowBar extends StatelessWidget implements libtokyo.WindowBar<Key, Widge
   @override
   Widget build(BuildContext context) {
     final height = AppBar.preferredHeightFor(context, Size.fromHeight(kToolbarHeight)) / 2;
+    
     final iconSize = ((IconTheme.of(context).size ?? 24) / height) * 15;
+    final iconPadding = iconSize * _ICON_PADDING_SIZE_MULT;
+
+    final leadingIconSize = ((IconTheme.of(context).size ?? 24) / height) * 20;
+    final leadingIconPadding = iconSize * _LEADING_ICON_PADDING_SIZE_MULT;
 
     return AppBar(
       automaticallyImplyLeading: false,
       backgroundColor: Theme.of(context).colorScheme.onSurface,
       leading: leading != null ? Padding(
-        padding: EdgeInsets.all(iconSize * 0.25),
+        padding: EdgeInsets.symmetric(vertical: iconPadding, horizontal: leadingIconPadding),
         child: leading!,
       ) : null,
-      leadingWidth: leading != null ? iconSize * 1.5 : null,
+      leadingWidth: leading != null ? (leadingIconPadding * 2.0) + leadingIconSize : null,
       titleSpacing: 0.0,
       title: title,
       titleTextStyle: Theme.of(context).textTheme.labelMedium,
