d58e127235f61c3f1e93a015c80cd30942560e04
Tristan Ross <tristan.ross@midstall.com>
2023-05-22 12:11:30 -0700

    chore(libtokyo): remove unused code

--- packages/libtokyo/lib/widgets.dart
+++ packages/libtokyo/lib/widgets.dart
@@ -1,4 +1,3 @@
 export 'package:libtokyo/widgets/app.dart';
-export 'package:libtokyo/widgets/button.dart';
 export 'package:libtokyo/widgets/scaffold.dart';
 export 'package:libtokyo/widgets/window_bar.dart';
